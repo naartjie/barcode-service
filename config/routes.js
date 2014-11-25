e56f1a67649d8bd733c4931544a5d3bf269c6f0a
Marcin Jekot <marcin@jekot.net>
2014-11-25 22:02:08 +0200

    using barcode-generator library (passing width x height)

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,25 @@
 'use strict';
 
+var bwip = require('bwip');
+
+var Barc = require('barcode-generator'),
+    barc = new Barc({hri: false});
+
 module.exports = function(app) {
 
-    app.get('/:type/:dimensions/:code', function(req, res) {
-        res.send('todo');
-    });
+    app.get('/code128/(:width)x(:height)/:code', function(req, res) {
 
+        var code = req.params.code,
+            width = req.params.width,
+            height = req.params.height;
+
+        var buf = barc.code128(code, width, height);
+
+        console.log(buf.length);
+
+        res
+            .status(200)
+            .set({ 'Content-Type':'image/png' })
+            .send(buf);
+    });
 };
